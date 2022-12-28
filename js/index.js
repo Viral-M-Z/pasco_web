77437e64312a168e42b0e73d5ca78e0fe43f6830
Viral-M-Z <vmzinzuwadia@gmail.com>
2022-12-28 13:19:58 +0530

    Changes

---
+++ js/index.js
@@ -0,0 +1,35 @@
+
+
+
+const slides = document.querySelectorAll(".mySlides")
+var counter = 0;
+// console.log(slides)
+
+
+slides.forEach(
+    (slide,index) => {
+        slide.style.left = `${index * 100}%`
+    }
+)
+
+const slideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter * 100}%)`
+        }
+    )
+}
+
+const goNext = () => {
+    counter++;
+    if (counter > slides.length-1) { counter = 0}
+    slideImage()
+}
+
+const goPrev = () => {
+    counter--;
+    if (counter < 0) {counter = slides.length-1}
+    slideImage()
+}
+
+
